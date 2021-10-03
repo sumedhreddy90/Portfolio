4426692b553b666dcbb4b71a89983df1391469b4
Sumedh Reddy <sumedhreddykoppula@gmail.com>
2021-10-03 14:21:12 -0400

    update in server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//imports
 const express = require('express')
-const portfolio = require('./routes/portfolio')
 const app = express()
-
+const port = process.env.PORT || 5000;
+const portfolio = require('./routes/portfolio')
+//static files
+app.use('/', express.static('public'))
+// Set Views
+app.set('views', './views')
 app.set('view engine', 'ejs')
+//Navigation
 // app.use('/portfolio'/ portfolio)
 app.get('/',(req, res)=>{
     res.render('index')
 })
-app.listen(7000)
+app.listen(port, () => console.info('App listening on port ${port}'))
